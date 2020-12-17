23747e3978d782302c33f0bc7e078c5c6d63421a
elafontelo <60907223+elafontelo@users.noreply.github.com>
2020-12-17 15:37:08 +0800

    Added initial files

---
+++ SampleApp/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using OpenPop.Mime;
+using OpenPop.Pop3;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SampleApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public void GetMessages()
+        {
+            //var client = new Pop3Client();
+            //client.Connect("pop.outlook.com", 995, true);
+            //client.Authenticate("ysraela.f@kinpo.com.ph", "kinpo123");
+
+            //var count = client.GetMessageCount();
+            //Message message = client.GetMessage(count);
+            //Console.WriteLine(message.Headers.Subject);
+        }
+
+    }
+}
